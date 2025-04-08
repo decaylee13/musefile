4ad53f6e99522891dcfb504c1ebc273e7469f3be
Decaylee13 <dl2635@princeton.edu>
2025-04-08 15:45:02 -0400

    nativewind, react-native installed

--- src/screens/DetailsScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export function DetailsScreen() {
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-600">Details Screen</Text>
-        </View>
-    );
-} 
